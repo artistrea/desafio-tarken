ff72659dd09a4243ddd617bfeb1fd190e04ef71a
artistrea <apiravesi@gmail.com>
2024-01-06 20:29:53 -0300

    feat: deleted message when removing from library

--- frontend/src/pages/MyLibrary.tsx
+++ frontend/src/pages/MyLibrary.tsx
@@ -1,10 +1,13 @@
-import { Grid, Typography } from "@mui/material";
+import { Alert, Grid, Stack, Typography } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useMoviesLibrary } from "../api/useMoviesLibrary";
 import { MovieCard } from "../components/MovieCard";
+import { useState } from "react";
+import { Movie } from "../api/useMoviesQuery";
 
 export function MyLibraryPage() {
   const { movies, setMovies } = useMoviesLibrary();
+  const [lastDeleted, setLastDeleted] = useState<Movie | undefined>(undefined);
 
   return (
     <div
@@ -12,9 +15,19 @@ export function MyLibraryPage() {
         display: "contents",
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Typography fontSize={26}>My Library</Typography>
-      </h1>
+      <Stack display={"flex"} flexDirection={"row"}>
+        <h1 style={{ margin: 0, paddingBlock: 6 }}>
+          <Typography fontSize={26}>My Library</Typography>
+        </h1>
+        {lastDeleted && (
+          <Alert
+            onClose={() => setLastDeleted(undefined)}
+            sx={{ marginLeft: "auto" }}
+          >
+            {lastDeleted.title} deleted from your watchlist
+          </Alert>
+        )}
+      </Stack>
       <main
         style={{
           padding: 16,
@@ -35,15 +48,10 @@ export function MyLibraryPage() {
                       )
                     )
                   }
-                  onRemoveFromLibrary={() =>
-                    setMovies((ms) =>
-                      ms.map((mv) =>
-                        mv.id === m.id
-                          ? { ...mv, hasAlreadyBeenAdded: false }
-                          : mv
-                      )
-                    )
-                  }
+                  onRemoveFromLibrary={() => {
+                    setLastDeleted(m);
+                    setMovies((ms) => ms.filter((mv) => mv.id !== m.id));
+                  }}
                   movie={m}
                 />
               </Grid>
